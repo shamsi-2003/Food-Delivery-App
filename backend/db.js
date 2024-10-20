4406f06212e62c332df0aed9b7291ae6fec7ec18
shamsi-2003 <139650670+shamsi-2003@users.noreply.github.com>
2024-10-20 17:21:53 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 // const mongoDbClient = require("mongodb").MongoClient
-const mongoURI = 'mongodb://<!#@!#@>:"!@!@"@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/Customer?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority' // Customer change url to your db you created in atlas
-// mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
-module.exports = function (callback) {
+const mongoURI = "mongodb://shamsikaif473:SxnHC0VGLm7YH4z3@cluster0-shard-00-00.0u7ny.mongodb.net:27017,cluster0-shard-00-01.0u7ny.mongodb.net:27017,cluster0-shard-00-02.0u7ny.mongodb.net:27017/gofoodmern?ssl=true&replicaSet=atlas-4zf5vs-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
     mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
         // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
         if (err) console.log("---" + err)
